2a9e08d6c225fcb036caf82569e72faeaf402bff
charles <abcd854884@gmail.com>
2023-10-06 16:36:08 +0800

    🧪 test: 新增純測試ws介面

--- Frontend/st_app/src/pages/test_page.vue
+++ Frontend/st_app/src/pages/test_page.vue
@@ -1,84 +1,178 @@
 <template>
-  <div class="demo">
-    <div style="margin-bottom: 30px">
-      <el-button @click="toggleSharing">
-        {{ enabled ? '停止' : '开始' }} 共享我的屏幕
-      </el-button>
-
-      <el-button v-if="enabled && !recording" @click="startRecording">
-        录制我的屏幕
-      </el-button>
-
-      <el-button v-if="recording" @click="stopRecording"> 停止录制 </el-button>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <!-- 麥克風按鈕 -->
+      <button @click="toggleMicrophone">
+        {{ isMicrophoneOn ? '關閉麥克風' : '開啟麥克風' }}
+      </button>
     </div>
-    <video ref="video" muted autoplay controls style="height: 40rem" />
   </div>
 </template>
 
 <script setup>
-  import { ref, watchEffect } from 'vue';
-  import { useDisplayMedia } from '@vueuse/core';
+  import { onBeforeMount, ref } from 'vue';
 
-  const video = ref(null);
-  const stopRecordingShow = ref(false);
-  const recording = ref(false);
-  const videoStreaming = ref([]);
 
-  const { stream, enabled } = useDisplayMedia({
-    video: true,
-  });
+  // - 語音傳輸
+  // > start
+  // WebSocket連接
+  const receivedMessage = ref(''); // 接收到的消息
+  let ws;
+  const init_ws_server = () => {
+    const socketUrl = `ws://localhost:8000/ws/${user_data.value.int_uid}`;
+    ws = new WebSocket(socketUrl);
 
-  watchEffect(() => {
-    if (video.value) {
-      video.value.srcObject = stream.value;
+    ws.onopen = () => {
+      console.log('WebSocket已連接');
+
+      // 在連結成功後，延遲3秒發送測試資料到後端
+      setTimeout(() => {
+        console.log('WebSocket狀態：', ws.readyState);
+        sendMessageToBackend();
+        startStreaming();
+      }, 3000);
+    };
+
+    // WebSocket接收到消息時執行
+    ws.onmessage = (event) => {
+      // 在receivedMessage中存储接收到的消息
+      receivedMessage.value = event.data;
+      console.log('接收到消息', event.data);
+    };
+
+    // WebSocket關閉時執行
+    ws.onclose = () => {
+      console.log('WebSocket已關閉');
+    };
+  };
+
+  // - 測試websocket連通
+  // > start
+  const testData = ref('這是測試資料'); // 您的測試資料
+
+  const sendMessageToBackend = () => {
+    // 檢查WebSocket是否已經初始化
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      // 發送測試資料到後端
+      ws.send(testData.value);
+      console.log('已發送測試資料到後端');
+    } else {
+      console.log('WebSocket未連接或尚未初始化');
     }
-  });
+  };
+  // > end
+
+  const isMicrophoneOn = ref(false); // 麥克風初始狀態為關閉
+
+  const toggleMicrophone = () => {
+    if (isMicrophoneOn.value) {
+      stopStreaming();
+      isMicrophoneOn.value = false;
+    } else {
+      init_ws_server();
+      isMicrophoneOn.value = true;
+    }
+  };
 
   let mediaRecorder;
 
-  const toggleSharing = () => {
-    if (enabled.value) {
-      stopRecordingShow.value = false;
-      if (recording.value) {
-        stopRecording();
+  const startStreaming = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      // 發送開始音頻串流的消息到後端
+      ws.send('start_audio_streaming');
+
+      // 啟動麥克風串流
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({
+            audio: {
+              sampleRate: 16000,
+              channelCount: 2,
+              volume: 1.0,
+            },
+          })
+          .then((stream) => {
+            if (ws && ws.readyState === WebSocket.OPEN) {
+              mediaRecorder = new MediaRecorder(stream);
+              mediaRecorder.ondataavailable = (event) => {
+                console.log('音頻數據：', event.data.size);
+                if (event.data.size > 0) {
+                  console.log('音頻傳輸中...');
+                  const audioBlob = new Blob([event.data], {
+                    type: 'audio/wav',
+                  });
+                  console.log(audioBlob);
+                  // - 將audioBlob存為檔案並提供下載
+                  // > start
+                  const url = URL.createObjectURL(audioBlob);
+                  const a = document.createElement('a');
+                  document.body.appendChild(a);
+                  a.style = 'display: none';
+                  a.href = url;
+                  a.download = 'audio.wav';
+                  a.click();
+                  window.URL.revokeObjectURL(url);
+                  // > end
+
+                  ws.send(audioBlob);
+                  // - 檢查ws.send是否成功
+                  // > start
+                  setTimeout(() => {
+                    if (ws.bufferedAmount === 0) {
+                      console.log('音頻數據已發送到後端');
+                    } else {
+                      console.log('音頻數據發送失敗');
+                    }
+                  }, 2000); // 增加等待時間（1秒），可以根據需要調整
+                }
+              };
+              mediaRecorder.start();
+              console.log('MediaRecorder狀態：', mediaRecorder.state);
+              // 更新界面按钮文字
+              isMicrophoneOn.value = true;
+            } else {
+              console.log('WebSocket未連接或尚未初始化');
+            }
+          })
+          .catch((error) => {
+            console.error('無法使用麥克風：', error);
+          });
       }
+    } else {
+      console.log('WebSocket未連接或尚未初始化');
     }
-    enabled.value = !enabled.value;
   };
 
-  const startRecording = () => {
-    stopRecordingShow.value = true;
-    recording.value = true;
-    videoStreaming.value = [];
-
-    mediaRecorder = new MediaRecorder(stream.value);
-
-    mediaRecorder.addEventListener('dataavailable', (event) => {
-      if (event.data.size > 0) {
-        videoStreaming.value.push(event.data);
+  const stopStreaming = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      // 發送停止音頻串流的消息到後端
+      ws.send('stopStreaming'); // 修改消息為 "stopStreaming"
+      // 停止MediaRecorder和關閉麥克風串流
+      if (isMicrophoneOn.value && mediaRecorder) {
+        // 停止MediaRecorder
+        mediaRecorder.stop();
+        // 更新界面按鈕文字
+        isMicrophoneOn.value = false;
       }
-    });
-
-    mediaRecorder.addEventListener('stop', () => {
-      const a = document.createElement('a');
-      const blob = new Blob(videoStreaming.value, { type: 'video/mp4' });
-      a.href = URL.createObjectURL(blob);
-      a.setAttribute('download', '录制视频.mp4');
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-
-      recording.value = false;
-    });
-
-    mediaRecorder.start();
-  };
-
-  const stopRecording = () => {
-    stopRecordingShow.value = false;
-    if (mediaRecorder) {
-      mediaRecorder.stop();
+    } else {
+      console.log('WebSocket未連接或尚未初始化');
     }
-    recording.value = false;
   };
+
+  // > end
+
+
+
+  onBeforeMount(() => {
+    init_user_data();
+  });
+  // > end
 </script>
+
+<style lang="scss">
+  .my-emoji {
+    vertical-align: middle;
+    height: 2em;
+    width: 2em;
+  }
+</style>
